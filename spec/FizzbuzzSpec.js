c1c4c656142718d28cf027dd52b237f6ebdd462a
armi1189 <armi1189@gmail.com>
2015-04-13 14:23:53 +0100

    make test context

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -1,4 +1,4 @@
-describe ("Fizzbuzz", function() {
+describe ("Check if is divisible", function() {
 
   var fizzbuzz = new Fizzbuzz();
 
@@ -10,10 +10,6 @@ describe ("Fizzbuzz", function() {
     expect(fizzbuzz.isDivisibleByThree(6)).toBe(true)
   });
 
-  it("should know if a number is not divisible by 3", function() {
-    expect(fizzbuzz.isDivisibleByThree(4)).toBe(false)
-  });
-
   it("should know if five is divisible by 5", function() {
     expect(fizzbuzz.isDivisibleByFive(5)).toBe(true)
   });
@@ -22,21 +18,37 @@ describe ("Fizzbuzz", function() {
     expect(fizzbuzz.isDivisibleByFive(20)).toBe(true)
   });
 
-  it("should know if number is not divisible by 5", function() {
-    expect(fizzbuzz.isDivisibleByFive(7)).toBe(false)
-  });
-
   it("should know if number is divisible by both 5 and 3", function() {
     expect(fizzbuzz.isDivisibleByFifthteen(15)).toBe(true)
+  });
+
+  it ("should know if any number is divisible by by both 5 and 3", function() {
+    expect(fizzbuzz.isDivisibleByFifthteen(30)).toBe(true)
+  });
+
+});
+
+describe ("Check if is not divisible", function() {
+
+  var fizzbuzz = new Fizzbuzz();
+
+  it("should know if a number is not divisible by 3", function() {
+    expect(fizzbuzz.isDivisibleByThree(4)).toBe(false)
+  });
+
+  it("should know if number is not divisible by 5", function() {
+    expect(fizzbuzz.isDivisibleByFive(7)).toBe(false)
   });
 
   it("should know if number is not divisible by both 5 and 3", function() {
     expect(fizzbuzz.isDivisibleByFifthteen(19)).toBe(false)
   });
 
-  it ("should know if any number is divisible by by both 5 and 3", function() {
-    expect(fizzbuzz.isDivisibleByFifthteen(30)).toBe(true)
-  });
+});
+
+describe ("Check the says method", function() {
+
+  var fizzbuzz = new Fizzbuzz();
 
   it ("should return Fizz when a number is divisible by 3", function() {
     expect(fizzbuzz.says(3)).toEqual("Fizz")
@@ -53,5 +65,6 @@ describe ("Fizzbuzz", function() {
   it ("should return the number when a number is not divisible for 15, 5, or 3", function() {
     expect(fizzbuzz.says(7)).toEqual(7)
   });
+  
 });
 
