2d64518b372c05a2c9beb496f8341cc2371413b8
armi1189 <armi1189@gmail.com>
2015-04-13 12:49:51 +0100

    knows if a number is not divisible by 5

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -6,7 +6,7 @@ describe ("Fizzbuzz", function() {
     expect(fizzbuzz.isDivisibleByThree(3)).toBe(true)
   });
 
-  it("should know if six is divisible by 3", function() {
+  it("should know if any number is divisible by 3", function() {
     expect(fizzbuzz.isDivisibleByThree(6)).toBe(true)
   });
 
@@ -14,12 +14,16 @@ describe ("Fizzbuzz", function() {
     expect(fizzbuzz.isDivisibleByThree(4)).toBe(false)
   });
 
-  it("should know if a number is divisible by 5", function() {
+  it("should know if five is divisible by 5", function() {
     expect(fizzbuzz.isDivisibleByFive(5)).toBe(true)
   });
 
+  it("should know if any number is divisible by 5", function() {
+    expect(fizzbuzz.isDivisibleByFive(20)).toBe(true)
+  });
+
   it("should know if number is not divisible by 5", function() {
-    expect(fizzbuzz.isDivisibleByFive(5)).toBe(false)
+    expect(fizzbuzz.isDivisibleByFive(7)).toBe(false)
   });
 });
 
--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -2,11 +2,12 @@ function Fizzbuzz() {
 };
 
 Fizzbuzz.prototype.isDivisibleByThree = function(num) {
-  if (num % 3 === 0)return true;
+  if (num % 3 === 0) return true;
   return false;
 };
 
 Fizzbuzz.prototype.isDivisibleByFive = function(num) {
-  return true
+  if (num === 5) return true;
+  return false;
 };
 
