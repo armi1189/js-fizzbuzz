f5378193591ec1a3ced776f68e4d38091fe05217
armi1189 <armi1189@gmail.com>
2015-04-13 12:14:12 +0100

    Know if a number is divisible by three

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -1,7 +1,13 @@
 describe ("Fizzbuzz", function() {
-  var fizbuzz = new Fizzbuzz();
+
+  var fizzbuzz = new Fizzbuzz();
+
   it("should know if a number is divisible by 3", function() {
     expect(fizzbuzz.isDivisibleByThree(3)).toBe(true)
+  });
+
+  it("should know if a number is not divisible by 3", function() {
+    expect(fizzbuzz.isDivisibleByThree(4)).toBe(false)
   });
 });
 
---
+++ src/Fizzbuzz.js
@@ -0,0 +1,6 @@
+function Fizzbuzz() {
+};
+
+Fizzbuzz.prototype.isDivisibleByThree = function(num) {
+  return true;
+};
